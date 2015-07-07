b26d31c59ca022d0ae1b37508b36fed58064abe6
Alexander A. Klimov <alexander.klimov@netways.de>
2015-07-07 09:46:13 +0200

    Add Vagrantfile and Puppet manifest

---
+++ .puppet/manifests/default.pp
@@ -0,0 +1,33 @@
+package { 'java':
+  name => 'java-1.7.0-openjdk',
+}
+
+yumrepo { 'logstash-1.5':
+  descr => 'logstash 1.5.x repository',
+  target => 'logstash.repo',
+  ensure => present,
+  baseurl => 'http://packages.elasticsearch.org/logstash/1.5/centos',
+  enabled => 1,
+  gpgkey => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+  repo_gpgcheck => 1,
+}
+-> package { 'logstash':
+  require => Package['java'],
+}
+
+yumrepo { 'elasticsearch-1.6':
+  descr => 'Elasticsearch 1.6.x repository',
+  target => 'elasticsearch.repo',
+  ensure => present,
+  baseurl => 'http://packages.elastic.co/elasticsearch/1.6/centos',
+  enabled => 1,
+  gpgkey => 'http://packages.elastic.co/GPG-KEY-elasticsearch',
+  repo_gpgcheck => 1,
+}
+-> package { 'elasticsearch':
+  require => Package['java'],
+}
+-> service { 'elasticsearch':
+  ensure => running,
+  enable => true,
+}
---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "puppetlabs/centos-6.6-64-puppet"
+
+  config.vm.box_check_update = false
+
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+  config.vm.synced_folder ".", "/vagrant"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = false
+    vb.memory = "1024"
+  end
+
+  config.vm.provision "puppet" do |puppet|
+    puppet.manifests_path = ".puppet/manifests"
+    puppet.manifest_file = "default.pp"
+  end
+end
