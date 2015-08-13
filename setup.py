64ee89c135fce7c22c9862edff7823e595cc0c94
Alexander A. Klimov <alexander.klimov@netways.de>
2015-08-13 16:58:13 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='elkproxy',
+    version='0.0',
+    author='NETWAYS GmbH',
+    author_email='info@netways.de',
+    description='a transparent proxy for securing Elasticsearch',
+    license='GPLv2+',
+    url='https://project.netways.de/projects/elk-proxy',
+    long_description='The ELK Proxy is a transparent HTTP proxy for securing '
+                     'Elasticsearch by permitting specific users to access only '
+                     'specific data.',
+    packages=['libelkproxy'],
+    zip_safe=False
+)
