73ce99f8ee8d04e0bdf48b7deefc420d9ff0d4b1
Alexander A. Klimov <alexander.klimov@netways.de>
2015-09-03 19:31:26 +0200

    Implement ScalarWrapper
    refs #18169

--- libelkproxy/util_json.py
+++ libelkproxy/util_json.py
@@ -16,7 +16,10 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-__all__ = ['unicode_to_str']
+import itertools
+
+
+__all__ = ['unicode_to_str', 'ScalarWrapper']
 
 
 def unicode_to_str(j, encoding='utf_8'):
@@ -34,3 +37,78 @@ def unicode_to_str(j, encoding='utf_8'):
         return [unicode_to_str(x, encoding=encoding) for x in j]
 
     return j.encode(encoding) if isinstance(j, unicode) else j
+
+
+class ScalarWrapper(object):
+    """
+    Wraps any object to diffecence between multiple instances with the same value at different positions
+
+    assert 1 is 1
+    assert ScalarWrapper(1) is not ScalarWrapper(1)
+    """
+
+    def __init__(self, obj):
+        """
+        :param obj: the object to wrap
+        """
+
+        self._obj = obj
+
+    def get_wrapped(self):
+        """
+        :return: the wrapped object
+        """
+
+        return self._obj
+
+    def __hash__(self):
+        return hash(self._obj)
+
+    def __eq__(self, other):
+        """
+        :rtype: bool
+        """
+
+        if not isinstance(other, type(self)):
+            return NotImplemented
+
+        return self._obj == other._obj
+
+    def __repr__(self):
+        """
+        :rtype: str
+        """
+
+        return '{0}({1!r})'.format(type(self).__name__, self._obj)
+
+    @classmethod
+    def wrap_recursive(cls, j):
+        """
+        Return a copy of j with all objects inside dict or list wrapped in ScalarWrapper
+
+        :param j: parsed JSON as returned by json.loads()
+        """
+
+        return cls(
+            dict((
+                itertools.imap(cls.wrap_recursive, x) for x in j.iteritems()
+            )) if isinstance(j, dict) else map(
+                cls.wrap_recursive, j
+            ) if isinstance(j, list) else j
+        )
+
+    @classmethod
+    def unwrap_recursive(cls, j):
+        """
+        The inverse of wrap_recursive()
+
+        :param j: wrapped JSON as returned by wrap_recursive()
+        :type j: ScalarWrapper
+        """
+
+        unwrapped = j.get_wrapped()
+        return dict((
+            itertools.imap(cls.unwrap_recursive, x) for x in unwrapped.iteritems()
+        )) if isinstance(unwrapped, dict) else map(
+            cls.unwrap_recursive, unwrapped
+        ) if isinstance(unwrapped, list) else unwrapped
